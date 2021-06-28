317b0f051609868b7a9b452f04b82296235b1143
Evaldas Doda <evaldasdoda@Evaldass-MacBook-Pro-2.local>
2021-06-28 14:55:24 +0300

    Added user profile feed API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -11,3 +11,13 @@ class UpdateOwnProfile(permissions.BasePermission):
 
         # Check if user id matches when trying to edit HTTP PUT/PATCH/DELETE, not SAFE_METHODS
         return obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """ Allow users to update their own status """
+    def has_object_permission(self, request, view, obj):
+        """ Check the user is trying to update their own status """
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
